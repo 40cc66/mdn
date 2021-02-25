c5f4929f5ec1327d6231a5508243326d057de800
Stanislav Akimov <39214862+akimovstv@users.noreply.github.com>
2021-02-25 19:05:55 +0200

    Add js

--- getting-started-with-the-web/scripts/main.js
+++ getting-started-with-the-web/scripts/main.js
@@ -1,2 +1,43 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = "Hello world!";
+let myImage = document.querySelector('img');
+let images = [
+    'images/peach-01.png',
+    'images/peach-02.png',
+    'images/peach-03.png',
+    'images/peach-04.png',
+    'images/peach-05.png',
+    'images/peach-06.png',
+]
+
+myImage.onclick = function () {
+    let currentSrc = myImage.getAttribute('src');
+    let newSrc;
+    do {
+        newSrc = images[Math.floor((Math.random() * images.length))];
+    } while (currentSrc === newSrc);
+    myImage.setAttribute('src', newSrc);
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = `Hello, ${myName}!`;
+    }
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Hello, ' + storedName + '!';
+}
+
+myButton.onclick = function () {
+    console.log('Click button');
+    setUserName();
+}
